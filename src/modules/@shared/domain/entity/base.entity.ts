afe309de691e4b34290d75d741e9e7462eecbd13
Rian Marlon <rianmarlon20@yahoo.com>
2025-01-12 15:05:34 -0300

    feat: add product entity

---
+++ src/modules/@shared/domain/entity/base.entity.ts
@@ -0,0 +1,25 @@
+import Id from "../value-object/id.value-object";
+
+export default class BaseEntity {
+  private _id: Id;
+  private _createdAt: Date;
+  private _updatedAt: Date;
+
+  constructor(id?: string) {
+    this._id = new Id(id);
+    this._createdAt = new Date();
+    this._updatedAt = new Date();
+  }
+
+  get id(): Id {
+    return this._id;
+  }
+
+  get createdAt(): Date {
+    return this._createdAt;
+  }
+
+  get updatedAt(): Date {
+    return this._updatedAt;
+  }
+}
