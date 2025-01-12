3f7369417a0383e519d40cc9c95a52a7ad415723
Rian Marlon <rianmarlon20@yahoo.com>
2025-01-12 18:25:45 -0300

    feat: add product-adm facade

---
+++ src/modules/product-adm/facade/product-adm.facade.interface.ts
@@ -0,0 +1,31 @@
+export interface AddProductFacadeInputDto {
+  id?: string;
+  name: string;
+  description: string;
+  purchasePrice: number;
+  stock: number;
+}
+
+export interface AddProductFacadeOutputDto {
+  id: string;
+  name: string;
+  description: string;
+  purchasePrice: number;
+  stock: number;
+}
+
+export interface CheckStockFacadeInputDto {
+  productId: string;
+}
+
+export interface CheckStockFacadeOutputDto {
+  productId: string;
+  stock: number;
+}
+
+export default interface ProductAdmFacadeInterface {
+  addProduct(input: AddProductFacadeInputDto): Promise<any>;
+  checkStock(
+    input: CheckStockFacadeInputDto
+  ): Promise<CheckStockFacadeOutputDto>;
+}
---
+++ src/modules/product-adm/facade/product-adm.facade.ts
@@ -0,0 +1,25 @@
+import UseCaseInterface from "../../@shared/usecase/use-case.interface";
+import ProductAdmFacadeInterface, {
+  AddProductFacadeInputDto,
+  AddProductFacadeOutputDto,
+  CheckStockFacadeInputDto,
+  CheckStockFacadeOutputDto,
+} from "./product-adm.facade.interface";
+
+export default class ProductAdmFacade implements ProductAdmFacadeInterface {
+  constructor(
+    private _addUsecase: UseCaseInterface,
+    private _checkStockUsecase: UseCaseInterface
+  ) {}
+
+  addProduct(
+    input: AddProductFacadeInputDto
+  ): Promise<AddProductFacadeOutputDto> {
+    return this._addUsecase.execute(input);
+  }
+  checkStock(
+    input: CheckStockFacadeInputDto
+  ): Promise<CheckStockFacadeOutputDto> {
+    return this._checkStockUsecase.execute(input);
+  }
+}
