f141e087c40834213ff34619bf29129ad3987176
Rian Marlon <rianmarlon20@yahoo.com>
2025-01-23 09:16:28 -0300

    feat: add invoice entity

---
+++ src/modules/invoice/domain/invoice-item.entity.ts
@@ -0,0 +1,27 @@
+import AggregateRoot from "../../@shared/domain/entity/aggregate-root.interface";
+import BaseEntity from "../../@shared/domain/entity/base.entity";
+
+type InvoiceItemProps = {
+  id?: string;
+  name: string;
+  price: number;
+};
+
+export default class InvoiceItem extends BaseEntity implements AggregateRoot {
+  private _name: string;
+  private _price: number;
+
+  constructor(props: InvoiceItemProps) {
+    super(props.id);
+    this._name = props.name;
+    this._price = props.price;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get price(): number {
+    return this._price;
+  }
+}
