60164d2378022d99d947614befd4ad06eaf0009a
Rian Marlon <rianmarlon20@yahoo.com>
2025-01-23 09:50:48 -0300

    feat: change find invoice usecase

--- src/modules/invoice/usecase/find-invoice/find-invoice.usecase.ts
+++ src/modules/invoice/usecase/find-invoice/find-invoice.usecase.ts
@@ -12,23 +12,19 @@ export default class FindInvoiceUseCase implements UseCaseInterface {
     input: FindInvoiceUseCaseInputDto
   ): Promise<FindInvoiceUseCaseOutputDto> {
     const invoice = await this._invoiceRepository.find(input.id);
-    let total = 0;
-    const items = invoice.items.map((item) => {
-      total += item.price;
-      return {
-        id: item.id.value,
-        name: item.name,
-        price: item.price,
-      };
-    });
-
     return {
       id: invoice.id.value,
       name: invoice.name,
       document: invoice.document,
       address: invoice.address,
-      items: items,
-      total: total,
+      items: invoice.items.map((item) => {
+        return {
+          id: item.id.value,
+          name: item.name,
+          price: item.price,
+        };
+      }),
+      total: invoice.total,
       createdAt: invoice.createdAt,
     };
   }
