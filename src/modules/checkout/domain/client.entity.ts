e5610fe827318f07665b5a7038062f5889c67a41
Rian Marlon <rianmarlon20@yahoo.com>
2025-01-27 09:48:01 -0300

    feat: add order entity

---
+++ src/modules/checkout/domain/client.entity.ts
@@ -0,0 +1,33 @@
+import BaseEntity from "../../@shared/domain/entity/base.entity";
+
+type ClientProps = {
+  id?: string;
+  name: string;
+  email: string;
+  address: string;
+};
+
+export default class Client extends BaseEntity {
+  private _name: string;
+  private _email: string;
+  private _address: string;
+
+  constructor(props: ClientProps) {
+    super(props.id);
+    this._name = props.name;
+    this._email = props.email;
+    this._address = props.address;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  get address(): string {
+    return this._address;
+  }
+}
---
+++ src/modules/checkout/domain/product.entity.ts
@@ -0,0 +1,33 @@
+import BaseEntity from "../../@shared/domain/entity/base.entity";
+
+type ProductProps = {
+  id?: string;
+  name: string;
+  description: string;
+  salesPrice: number;
+};
+
+export default class Product extends BaseEntity {
+  private _name: string;
+  private _description: string;
+  private _salesPrice: number;
+
+  constructor(props: ProductProps) {
+    super(props.id);
+    this._name = props.name;
+    this._description = props.description;
+    this._salesPrice = props.salesPrice;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  get salesPrice(): number {
+    return this._salesPrice;
+  }
+}
