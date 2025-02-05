030b53cbe8e8dfce5442e3630703755ac5278a0f
Rian Marlon <rianmarlon20@yahoo.com>
2025-02-05 09:33:46 -0300

    feat: add migration to create table products

---
+++ src/@shared/infra/database/sequelize/migrations/20250203105045-create-table-products.ts
@@ -0,0 +1,46 @@
+import { Sequelize, DataTypes } from "sequelize";
+import { MigrationFn } from "umzug";
+
+export const up: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().createTable("products", {
+    id: {
+      primaryKey: true,
+      type: DataTypes.STRING(36),
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    salesPrice: {
+      type: DataTypes.NUMBER({ precision: 10, scale: 2 }),
+      allowNull: true,
+    },
+    purchasePrice: {
+      type: DataTypes.NUMBER({ precision: 10, scale: 2 }),
+      allowNull: true,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+  });
+};
+
+export const down: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().dropTable("products");
+};
