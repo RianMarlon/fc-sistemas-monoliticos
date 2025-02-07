e7b820c568c5776d70842c007f5b7724ddb486d2
Rian Marlon <rianmarlon20@yahoo.com>
2025-02-07 20:41:13 -0300

    chore: ignore migrations folder in coverage

--- jest.config.ts
+++ jest.config.ts
@@ -31,9 +31,7 @@ const config: Config = {
   // coverageDirectory: undefined,
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ["/node_modules/", "/migrations/"],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
