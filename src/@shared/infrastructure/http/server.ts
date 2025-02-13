a7ffdc93d759c9d6f7f9b374636f77288ae91f59
Rian Marlon <rianmarlon20@yahoo.com>
2025-02-12 22:19:53 -0300

    feat: add sequelize connection config

--- src/@shared/infrastructure/http/server.ts
+++ src/@shared/infrastructure/http/server.ts
@@ -1,9 +1,19 @@
+import { Sequelize } from "sequelize-typescript";
+import { join } from "path";
+
 import fastifyServer from "./fastify";
 
 const start = async () => {
+  const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: join(__dirname, "../../../../sqlite.db"),
+    models: [join(__dirname, "../../../**/*.model.{ts,js}")],
+  });
   try {
+    await sequelize.authenticate();
     await fastifyServer.listen({ port: Number(process.env.PORT || 3000) });
   } catch (err) {
+    await sequelize.close();
     console.error(err);
     process.exit(1);
   }
